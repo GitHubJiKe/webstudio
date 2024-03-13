37a3e74c551f1acdd28c9dfea968e9bdb704cb22
yuanpengfei03 <yuanpengfei03@meituan.com>
2024-03-13 19:04:58 +0800

    codemirror demo

--- src/apps/Demo/App.vue
+++ src/apps/Demo/App.vue
@@ -1,11 +1,80 @@
 <script setup lang="ts">
+import { EditorState } from '@codemirror/state';
+import { EditorView, basicSetup } from 'codemirror';
+import { Diagnostic, lintKeymap } from '@codemirror/lint';
+import { onMounted } from 'vue';
+import * as sql from '@codemirror/lang-sql';
+import { oneDark } from '@codemirror/theme-one-dark';
+
+import {
+  keymap,
+} from '@codemirror/view'
+
+async function sqlLinter(view: EditorView) {
+  let diagnostics: Diagnostic[] = []
+  // @ts-ignore
+  const text = view.state.doc.text[0]
+  console.log(view.state, text);
+  // @ts-ignore
+  const parser = new window.NodeSQLParser.Parser();
+  try {
+    const ast = parser.astify(text);
+    console.log("ast::", ast);
+    const sql = parser.sqlify(ast);
+    console.log("sql::", sql);
+  } catch (error) {
+    console.error("error::", error)
+    // @ts-ignore
+    diagnostics.push({
+      // @ts-ignore
+      from: error.location.start.offset,
+      // @ts-ignore
+      to: error.location.end.offset,
+      severity: "error",
+      // @ts-ignore
+      message: error.message,
+    });
+  }
+  return diagnostics
+
+
+}
+
+// Set up the CodeMirror editor with linting
+const state = EditorState.create({
+  doc: `select * from user wher name="小明"`,
+  extensions: [oneDark,
+    basicSetup,
+    // @ts-ignore
+    // linter(sqlLinter),
+    // lintGutter(),
+    sql.sql(),
+    // ... other extensions like language support
+    keymap.of([
+      ...lintKeymap
+    ])
+  ]
+});
+
+onMounted(() => {
+  const view = new EditorView({
+    state,
+    parent: document.body.querySelector('#sql')!
+  });
+
+  console.log(view);
+})
+
 </script>
 
 <template>
-  <div class="m-1">
+  <div class="m-1 bg-white">
     <h1 class="p-0 m-0">
       Demo
     </h1>
+    <div id="sql">
+
+    </div>
   </div>
 </template>
 
